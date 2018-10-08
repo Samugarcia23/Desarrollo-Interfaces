d984b141973da78897911990a2e5a1f70689020e
Sam <samugarcia23@gmail.com>
2018-10-08 12:43:24 +0200

    Ejercicio Botones y Propiedades

--- 04-Botones_y_Propiedades/04-Botones_y_Propiedades/04-Botones_y_Propiedades/MainPage.xaml.cs
+++ 04-Botones_y_Propiedades/04-Botones_y_Propiedades/04-Botones_y_Propiedades/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -16,7 +17,7 @@ using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
 
-namespace _03_HelloWorld_WU
+namespace _04_Botones_y_Propiedades
 {
     /// <summary>
     /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
@@ -26,20 +27,26 @@ namespace _03_HelloWorld_WU
         public MainPage()
         {
             this.InitializeComponent();
-        }
+			Button btn3;
+			btn3 = new Button();
+			btn3.Content = "Boton 3";
+			btn3.Background = new SolidColorBrush(Colors.Blue);
+			btn3.Foreground = new SolidColorBrush(Colors.White);
+			btn3.HorizontalAlignment = HorizontalAlignment.Center;
+			btn3.VerticalAlignment = VerticalAlignment.Center;
+			btn3.Height = 70;
+			btn3.Width = 200;
+			FontFamily family = new FontFamily("Verdana");
+			btn3.FontFamily = family;
+			btn3.FontSize = 16;
+			btn3.BorderBrush = new SolidColorBrush(Colors.Yellow);
+			stackPanel1.Children.Add(btn3);
+			btn3.Click += btn3_Click;
+		}
 
-		/// <summary>
-		///		Este metodo lanza un dialog en el que se muestra un nombre escrito en el textbox(tbxNombre)
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="e"></param>
-
-		private async void btnSaludo_Click(object sender, RoutedEventArgs e)
+		public async void btn3_Click(object sender, RoutedEventArgs e)
 		{
-			String nombre;
-			nombre = tbxNombre.Text;
-
-			MessageDialog msgbox = new MessageDialog($"Hola {nombre}");
+			MessageDialog msgbox = new MessageDialog($"Hola, has pulsado el boton 3");
 
 			msgbox.Commands.Clear();
 			msgbox.Commands.Add(new UICommand { Label = "Salir", Id = 0 });
@@ -47,9 +54,9 @@ namespace _03_HelloWorld_WU
 			var res = await msgbox.ShowAsync();
 			if ((int)res.Id == 0)
 			{
-				MessageDialog msgbox2 = new MessageDialog("", $"Adios! } :)");
+				MessageDialog msgbox2 = new MessageDialog("", $"Adios! :)");
 				await msgbox2.ShowAsync();
 			}
 		}
-	}
+    }
 }
