c899a8300574efccdf2796b8fb969689400a19c3
Sam <samugarcia23@gmail.com>
2018-10-20 17:56:41 +0200

    Ejercicio 9 Terminado

--- 09-EjercicioControles-WU/09-EjercicioControles-WU/MainPage.xaml.cs
+++ 09-EjercicioControles-WU/09-EjercicioControles-WU/MainPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
@@ -29,7 +30,40 @@ namespace _09_EjercicioControles_WU
 
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
-			this.Frame.Navigate(typeof(Pagina2));
+			Frame.Navigate(typeof(Pagina2));
 		}
+
+		#region Animación 
+
+		/*Método que conecta la imagen que realizará la animación con la imagen de destino*/
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			//Base representa el contenido al que puede navegar un control "Frame"
+			base.OnNavigatedTo(e);
+
+			/*Se obtiene la animación que hemos creado anteriormente y si no es NULL entonces se utiliza 
+			 TryStart(), pasándole el nombre de la imagen destino para comenzar*/
+			ConnectedAnimation animation =
+			ConnectedAnimationService.GetForCurrentView().GetAnimation("forwardAnimation");
+			if (animation != null)
+			{
+				animation.TryStart(DestinationImage);
+			}
+		}
+		#endregion
+
 	}
 }
+
+/*Principal uso de las Animaciones Conectadas:
+ * 
+ * Las animaciones conectadas permiten crear una experiencia de navegación dinámica y atractiva al animar 
+ * la transición de un elemento entre dos vistas distintas. 
+ * Esto ayuda al usuario a mantener el contexto y ofrece continuidad entre las vistas.
+ * En una animación conectada, un elemento parece "Continuar" entre dos vistas durante un cambio 
+ * en el contenido de la interfaz de usuario, volando por la pantalla desde su ubicación en la vista de origen 
+ * hasta su destino en la nueva vista.
+ * Esto enfatiza el contenido común entre las vistas y 
+ * crea un efecto atractivo y dinámico como parte de una transición.
+ * 
+ */
---
+++ 09-EjercicioControles-WU/09-EjercicioControles-WU/Pagina2.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace _09_EjercicioControles_WU
+{
+	/// <summary>
+	/// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+	/// </summary>
+	public sealed partial class Pagina2 : Page
+	{
+		public Pagina2()
+		{
+			this.InitializeComponent();
+		}
+		#region Animacion
+		//Funcion que al hacer click en la imagen, nos llevara hacia la pagina donde esta la imagen de destino
+		private void SourceImage_PointerPressed(object sender, PointerRoutedEventArgs e)
+		{
+			/*SuppressNavigationTransitionInfo() suprime la animacion por defecto para evitar conflicto con la nueva
+			animacion*/
+			Frame.Navigate(typeof(MainPage), null, new SuppressNavigationTransitionInfo());
+		}
+
+		/*Funcion que prepara el contexto actual para realizar la animación, forwardAnimation es la animación 
+		  hacia delante por defecto (se puede hacer una personalizada)*/
+		protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+		{
+			ConnectedAnimationService.GetForCurrentView()
+				.PrepareToAnimate("forwardAnimation", SourceImage);
+			// Para una animación personalizada, habría que usar:
+			// animation.Configuration = new BasicConnectedAnimationConfiguration();
+		}
+		#endregion
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			if (Frame.CanGoBack)
+				Frame.GoBack();
+		}
+	}
+}
