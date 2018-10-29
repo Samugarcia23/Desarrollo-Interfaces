f4dd82a812217dacfc6b6d5923e99d3d775bd2f3
Sam <samugarcia23@gmail.com>
2018-10-29 14:38:08 +0100

    Ejercicio 12 Terminado

--- 12-SplitView-UWP/12-SplitView-UWP/MainPage.xaml.cs
+++ 12-SplitView-UWP/12-SplitView-UWP/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _12_SplitView_UWP.Views;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -26,5 +27,59 @@ namespace _12_SplitView_UWP
         {
             this.InitializeComponent();
         }
-    }
+
+		private void btnHome_Click(object sender, RoutedEventArgs e)
+		{
+			contenedor.Navigate(typeof(Pagina1));
+		}
+
+		private void btnMensajes_Click(object sender, RoutedEventArgs e)
+		{
+			contenedor.Navigate(typeof(Pagina2));
+		}
+
+		private void btnAvisos_Click(object sender, RoutedEventArgs e)
+		{
+			contenedor.Navigate(typeof(Pagina3));
+		}
+
+		private void btnContactos_Click(object sender, RoutedEventArgs e)
+		{
+			contenedor.Navigate(typeof(Pagina4));
+		}
+
+		private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+		{
+			splView.IsPaneOpen = !splView.IsPaneOpen;
+		}
+
+		private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (btnPagina1.IsSelected)
+			{
+				btnHome_Click(sender, e);
+			}
+			else
+			{
+				if (btnPagina2.IsSelected)
+				{
+					btnMensajes_Click(sender, e);
+				}
+				else
+				{
+					if (btnPagina3.IsSelected)
+					{
+						btnAvisos_Click(sender, e);
+					}
+					else
+					{
+						if (btnPagina4.IsSelected)
+						{
+							btnContactos_Click(sender, e);
+						}
+					}
+				}
+			}
+		}
+	}
 }
