dbdb208e23d0ab89def9a9377a92aa17ea3d7cc6
Sam <samugarcia23@gmail.com>
2018-11-08 14:39:59 +0100

    Ejercicio CRUD UWP creado

---
+++ 15-CRUDPersonas-UWP/15-CRUDPersonas-Entidades/clsDepartamento.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _15_CRUDPersonas_Entidades
+{
+	public class clsDepartamento
+	{
+		#region Constructor por Defecto
+		public clsDepartamento()
+		{
+
+		}
+		#endregion
+		#region Constructor por parametros
+		public clsDepartamento(int idDepartamento, String nombreDepartamento)
+		{
+			this.idDepartamento = idDepartamento;
+			this.nombreDepartamento = nombreDepartamento;
+		}
+		#endregion
+		#region atributos_Departamento
+		public int idDepartamento { get; set; }
+		public String nombreDepartamento { get; set; }
+		#endregion
+	}
+}
---
+++ 15-CRUDPersonas-UWP/15-CRUDPersonas-UI/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,64 @@
+﻿using _15_CRUDPersonas_Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_CRUDPersonas_UI.ViewModels
+{
+	public class MainPageViewModel : INotifyPropertyChanged
+	{
+		#region Propiedades Privadas
+		private List<clsPersona> _ListadoDePersonas;
+		private clsPersona _PersonaSeleccionada;
+		private List<clsDepartamento> _ListadoDepartamentos;
+		#endregion
+
+		#region Propiedades Publicas
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public List<clsPersona> ListadoDePersonas
+		{
+			get { return _ListadoDePersonas; }
+			set { _ListadoDePersonas = value; }
+		}
+
+		public clsPersona PersonaSeleccionada
+		{
+			get { return _PersonaSeleccionada; }
+			set { _PersonaSeleccionada = value; NotifyPropertyChanged("PersonaSeleccionada"); }
+		}
+
+		public List<clsDepartamento> ListadoDepartamentos
+		{
+			get { return _ListadoDepartamentos; }
+			set { _ListadoDepartamentos = value;}	
+		}
+
+		#endregion
+
+		#region Constructores
+		public MainPageViewModel()
+		{
+			//Cargar el listado de Personas
+			//_ListadoDePersonas = Listado de la capa bl .listadoPersona();
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Metodo que crea un nuevo PropertyChangedEventArgs para realizar el cambio en los datos del formulario
+		/// </summary>
+		/// <param name="nombre"></param>
+		#region Otros
+		protected void NotifyPropertyChanged(string nombre)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
+		}
+		#endregion
+
+	}
+}
