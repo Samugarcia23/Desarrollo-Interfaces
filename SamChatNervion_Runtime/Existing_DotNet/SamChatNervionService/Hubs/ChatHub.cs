d93d6b237a814fe161df6df18323cea4e9d6df7b
Sam <samugarcia23@gmail.com>
2019-01-10 14:40:48 +0100

    Chat SignalR

---
+++ SamChatNervion_Runtime/Existing_DotNet/SamChatNervionService/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+using SamChatNervionService.DataObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SamChatNervionService.Hubs
+{
+	public class ChatHub : Hub
+	{
+		public void Send(ChatMessge message)
+		{
+			Clients.All.broadcastMessage(message);
+		}
+	}
+}
